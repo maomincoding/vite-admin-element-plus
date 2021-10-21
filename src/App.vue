156a80ef413fc91a58b436406d539b79675c6949
maomincoding <17864296568@163.com>
2021-10-21 16:18:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <router-view />
+    <el-config-provider :locale="locale">
+      <router-view />
+    </el-config-provider>
   </div>
 </template>
 
 <script>
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 export default {
   name: 'App',
+  setup() {
+    return {
+      locale: zhCn,
+    };
+  },
 };
 </script>
